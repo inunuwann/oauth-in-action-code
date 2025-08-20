2786dc1243ceadbfc014055f725dd9f2930c320f
inunuwann <yukiya.0216.5063@gmail.com>
2025-08-20 19:43:14 +0900

    complete ch-4-ex-1

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -23,10 +23,40 @@ var resource = {
 };
 
 var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
-	
+	var inToken = null;
+	var auth = req.headers['authorization']
+
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+		inToken = auth.slice('bearer '.length)
+	} else if (req.body && req.body.access_token) {
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token) {
+		inToken = req.query.access_token;
+	}
+
+	console.log('Incoming token: %s', inToken);
+
+	nosql.one().make(function(builder) {
+		builder.where('access_token', inToken);
+	  	builder.callback(function(err, token) {
+			if (token) {
+				console.log("We found a matching token: %s", inToken);
+			} else {
+				console.log('No matching token was found.');
+			};
+	    req.access_token = token;
+	    next();
+	    return;
+	  });
+	});
+};
+
+var requireAccessToken = function(req, res, next) {
+    if (req.acess_token) {
+      next();
+    } else {
+      res.status(401).end();
+    }
 };
 
 app.options('/resource', cors());
@@ -35,12 +65,15 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
-
+app.post("/resource", getAccessToken, function(req, res){
+	if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
 	/*
 	 * Check to see if the access token was found or not
 	 */
-	
 });
 
 var server = app.listen(9002, 'localhost', function () {
@@ -49,4 +82,3 @@ var server = app.listen(9002, 'localhost', function () {
 
   console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
